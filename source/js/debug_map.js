faa364ea27d68b5779e29e084bb86b1dd7e32e18
VillagerN4 <villnumber4@gmail.com>
2026-01-08 20:32:55 +0100

    Merge clean up.

--- source/js/debug_map.js
+++ source/js/debug_map.js
@@ -20,6 +20,20 @@ function createDebugMap(){
             let neighbors = getHexNeighbors(r, c);
             let forests = 0;
             let roughs = 0;
+
+            const c_css = {
+                position: "absolute",
+                left: x,
+                top: y,
+                width: hexRadius*2 + "px",
+                height: hexHeight*2 + "px"
+            };
+
+            const l_css = {
+                width: hexRadius*2 + "px",
+                'font-size': "8px"
+            };
+
             neighbors.forEach(n => {
                 if(board_data.board[n[1]][n[0]].terrainType == 2) forests++;
                 if(board_data.board[n[1]][n[0]].terrainType == 1) roughs++;
@@ -31,12 +45,8 @@ function createDebugMap(){
                     class: "debug_village_display",
                     src: "assets/map/village_houses_" + Math.round(Math.random() * 2) + ".png",
                     css: {
-                        position: "absolute",
-                        transform: `rotate(${60 * (-1 + Math.floor(Math.random())*2)}deg)`,
-                        left: x,
-                        top: y,
-                        width: hexRadius*2 + "px",
-                        height: hexHeight*2 + "px"
+                        ...c_css,
+                        transform: `rotate(${60 * (-1 + Math.floor(Math.random())*2)}deg)`
                     }
                 }).appendTo('#debug_board_container');
             }
@@ -45,13 +55,7 @@ function createDebugMap(){
                 id: padLeft(c + 1, 2) + padLeft(r, 2),
                 class: "debug_hex_display",
                 src: "assets/map/terrain/" + cell.terrainType + (cell.terrainType == 0 ? "_" + Math.floor((forests + roughs)/2) : "") + ".png",
-                css: {
-                    position: "absolute",
-                    left: x,
-                    top: y,
-                    width: hexRadius*2 + "px",
-                    height: hexHeight*2 + "px"
-                }
+                css: c_css
             }).appendTo('#debug_board_container');
 
             if(cell.terrainType == 2){
@@ -60,11 +64,7 @@ function createDebugMap(){
                     class: "debug_tree_display",
                     src: "assets/map/terrain/2_" + Math.round(Math.random() * 2) + "_" + forests + ".png",
                     css: {
-                        position: "absolute",
-                        left: x,
-                        top: y,
-                        width: hexRadius*2 + "px",
-                        height: hexHeight*2 + "px",
+                        ...c_css,
                         filter: `opacity(${forests*3 - Math.random()*5 + 82}%)`
                     }
                 }).appendTo('#debug_board_container');
@@ -75,36 +75,8 @@ function createDebugMap(){
                         class: "debug_tree_display",
                         src: "assets/map/terrain/_" + Math.round(Math.random() * 2) + "_" + Math.floor(forests) + ".png",
                         css: {
-                            position: "absolute",
-                            left: x,
-                            top: y,
-                            width: hexRadius*2 + "px",
-                            height: hexHeight*2 + "px",
+                            ...c_css,
                             filter: `opacity(${forests*3 + 66}%)`
-                        }
-                    }).appendTo('#debug_board_container');
-                }
-            }
-
-            if (cell.units && cell.units.length > 0) {
-
-                let unit = cell.units.find(u =>
-                    ["nazis", "brits", "us"].includes(u.faction)
-                );
-
-                if (unit) {
-                    let faction = faction;
-                    jQuery('<img>', {
-                        id: "unit_" + padLeft(c + 1, 2) + padLeft(r, 2),
-                        class: "unit_display",
-                        src: `assets/cell/${faction}/${faction}_standard.png`,
-                        css: {
-                            position: "absolute",
-                            left: x,
-                            top: y,
-                            pointerEvents: "none",
-                            width: hexRadius*2 + "px",
-                            height: hexHeight*2 + "px"
                         }
                     }).appendTo('#debug_board_container');
                 }
@@ -127,13 +99,7 @@ function createDebugMap(){
                         id: "houses" + padLeft(c + 1, 2) + padLeft(r, 2) + i,
                         class: "debug_house_display",
                         src: `assets/map/terrain/3_${i}.png`,
-                        css: {
-                            position: "absolute",
-                            left: x,
-                            top: y,
-                            width: hexRadius*2 + "px",
-                            height: hexHeight*2 + "px"
-                        }
+                        css: c_css
                     }).appendTo("#debug_board_container");
 
             };
@@ -142,33 +108,21 @@ function createDebugMap(){
             jQuery('<div>', {
                 id: "labeldiv" + padLeft(c + 1, 2) + padLeft(r, 2),
                 class: "debug_cell_label_display",
-                css: {
-                    position: "absolute",
-                    left: x,
-                    top: y,
-                    width: hexRadius*2 + "px",
-                    height: hexHeight*2 + "px"
-                }
+                css: c_css
             }).appendTo('#debug_board_container');
 
             jQuery('<p>', {
                 id: "label_cell" + padLeft(c + 1, 2) + padLeft(r, 2),
                 class: "debug_cell_label_text",
                 text: padLeft(c + 1, 2) + padLeft(r, 2),
-                css: {
-                    width: hexRadius*2 + "px",
-                    'font-size': "8px"
-                }
+                css: l_css
             }).appendTo('#' + "labeldiv" + padLeft(c + 1, 2) + padLeft(r, 2));
 
             jQuery('<p>', {
                 id: "label_cell_inner" + padLeft(c + 1, 2) + padLeft(r, 2),
                 class: "debug_cell_label_text_inner",
                 text: padLeft(c + 1, 2) + padLeft(r, 2),
-                css: {
-                    width: hexRadius*2 + "px",
-                    'font-size': "8px"
-                }
+                css: l_css
             }).appendTo('#' + "labeldiv" + padLeft(c + 1, 2) + padLeft(r, 2));
         }
     }
@@ -186,97 +140,57 @@ function updateDebugMap(){
             let cell_pos = getHexCenterPos(r, c);
             let x = cell_pos[0] - hexRadius*zoom + "px";
             let y = cell_pos[1] - hexHeight*zoom  + "px";
-            $("#village_marker" + padLeft(c + 1, 2) + padLeft(r, 2)).css({
+
+            const c_css = {
                 "width": hexRadius*2*zoom + "px",
                 "height": hexHeight*2*zoom + "px", 
                 "left": x,
                 "top": y
-            });
-            $("#" + padLeft(c + 1, 2) + padLeft(r, 2)).css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": x,
-                "top": y
-            });
-            $("#trees" + padLeft(c + 1, 2) + padLeft(r, 2)).css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": x,
-                "top": y
-            });
+            };
+
+            const l_css = {
+                "width": hexRadius*2*zoom + "px",
+                "font-size": 8*Math.sqrt(zoom) + "px"
+            };
+
+            $("#village_marker" + padLeft(c + 1, 2) + padLeft(r, 2)).css(c_css);
+            $("#" + padLeft(c + 1, 2) + padLeft(r, 2)).css(c_css);
+            $("#trees" + padLeft(c + 1, 2) + padLeft(r, 2)).css(c_css);
             for(let i = 0; i < 6; i++) {
                 if(cell.terrainType == 3){
-                    $("#" + "houses" + padLeft(c + 1, 2) + padLeft(r, 2) + i).css({
-                        "width": hexRadius*2*zoom + "px", 
-                        "height": hexHeight*2*zoom + "px", 
-                        "left": x,
-                        "top": y
-                    });
+                    $("#" + "houses" + padLeft(c + 1, 2) + padLeft(r, 2) + i).css(c_css);
                 }
             }
             
             cell.highways.forEach(edge => {
-            $("#" + "highway" + padLeft(c + 1, 2) + padLeft(r, 2) + edge).css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": x,
-                "top": y
-            });
+            $("#" + "highway" + padLeft(c + 1, 2) + padLeft(r, 2) + edge).css(c_css);
             });
             cell.roads.forEach(edge => {
-            $("#" + "road" + padLeft(c + 1, 2) + padLeft(r, 2) + edge).css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": x,
-                "top": y
+            $("#" + "road" + padLeft(c + 1, 2) + padLeft(r, 2) + edge).css(c_css);
             });
-            });
-            $("#labeldiv" + padLeft(c + 1, 2) + padLeft(r, 2)).css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": x,
-                "top": y
-            });
-            $("#label_cell" + padLeft(c + 1, 2) + padLeft(r, 2)).css({
-                "width": hexRadius*2*zoom + "px",
-                "font-size": 8*Math.sqrt(zoom) + "px"
-            });
-            $("#label_cell_inner" + padLeft(c + 1, 2) + padLeft(r, 2)).css({
-                "width": hexRadius*2*zoom + "px",
-                "font-size": 8*Math.sqrt(zoom)  + "px"
-            });
+            $("#labeldiv" + padLeft(c + 1, 2) + padLeft(r, 2)).css(c_css);
+
+            $("#label_cell" + padLeft(c + 1, 2) + padLeft(r, 2)).css(l_css);
+            $("#label_cell_inner" + padLeft(c + 1, 2) + padLeft(r, 2)).css(l_css);
 
             for (let id in gameState.units) {
-              const u = gameState.units[id];
-              const pos = getHexCenterPos(u.row, u.col);
-              const ux = pos[0] - hexRadius * zoom + "px";
-              const uy = pos[1] - hexHeight * zoom + "px";
+                const u = gameState.units[id];
+                const pos = getHexCenterPos(u.row, u.col);
+                const ux = pos[0] - hexRadius * zoom + "px";
+                const uy = pos[1] - hexHeight * zoom + "px";
 
-              $("#unit_" + id).css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": ux,
-                "top": uy
-            });
-            $("#unit_" + id + "_outline").css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": ux,
-                "top": uy
-            });
-            $("#unit_" + id + "_hull").css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": ux,
-                "top": uy
-            });
-            $("#unit_" + id + "_turret").css({
-                "width": hexRadius*2*zoom + "px", 
-                "height": hexHeight*2*zoom + "px", 
-                "left": ux,
-                "top": uy
-            });
-          }
+                const u_css = {
+                    "width": hexRadius*2*zoom + "px",
+                    "height": hexHeight*2*zoom + "px", 
+                    "left": ux,
+                    "top": uy
+                };
+
+                $("#unit_" + id).css(u_css);
+                $("#unit_" + id + "_outline").css(u_css);
+                $("#unit_" + id + "_hull").css(u_css);
+                $("#unit_" + id + "_turret").css(u_css);
+            }
         }
     }
 }
